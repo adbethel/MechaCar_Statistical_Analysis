844f1a95b32e8c2bcd92635f7ebcb5ff01be899d
adbethel <112285856+adbethel@users.noreply.github.com>
2022-12-11 15:38:08 -0500

    Update MechaCarChallenge.R

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,7 @@ summary(lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + 
 SusCoil <- read.csv(file="Suspension_Coil.csv",check.names=F, stringsAsFactors = F)
 total_summary <- SusCoil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = "keep")
 lot_summary <- SusCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = "keep")
+t.test(SusCoil$PSI,mu=mean(SusCoil$PSI))
+t.test(subset(SusCoil$PSI,SusCoil$Manufacturing_Lot =="Lot1"),mu=mean(SusCoil$PSI))
+t.test(subset(SusCoil$PSI,SusCoil$Manufacturing_Lot =="Lot2"),mu=mean(SusCoil$PSI))
+t.test(subset(SusCoil$PSI,SusCoil$Manufacturing_Lot =="Lot3"),mu=mean(SusCoil$PSI))
